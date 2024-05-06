8ead7c31648646fad0db8001d528aca04420e891
Huzerovo <huzerovo@outlook.com>
2024-05-06 23:32:25 +0800

    others: 替罪羊树算法源文件整理

---
+++ c-scapegoat-tree/main.c
@@ -0,0 +1,24 @@
+#include "btree.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main()
+{
+    int root  = 0;
+    int count = 0;
+    int seed  = time(NULL);
+    for (int i = 1; i < MAXN; i++) {
+        seed = seed * (count + (count + 1) * 7);
+        srand(seed);
+        count++;
+        insert_node(&root, floor(rand() * 100));
+        usleep(100);
+    }
+    printf("insert %d, rebuild %d, pro: %g%%\n", count, get_rebc(),
+           ((double)get_rebc() / (double)count) * 100);
+    return 0;
+}
